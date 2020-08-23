00b10e6ff77e01d6c71f6f6f660c69b979ce92fc
Biju Khatri Dhungel <bijudhungel1111@gmail.com>
2020-08-23 21:34:37 +0545

    checkout-form

--- src/components/Cart.js
+++ src/components/Cart.js
@@ -1,8 +1,41 @@
 import React, { Component } from "react";
 import "./Cart.css";
 export default class Cart extends Component {
+  state = {
+    showCheckout: false,
+    name: "",
+    email: "",
+    address: "",
+  };
+  handleClick = () => {
+    this.setState({ showCheckout: !this.state.showCheckout });
+  };
+
+  handleInput = (e) => {
+    const { name, value } = e.target;
+    this.setState({
+      [name]: value,
+    });
+  };
+  createOrder = (e) => {
+    e.preventDefault();
+    const order = {
+      name: this.state.name,
+      email: this.state.email,
+      address: this.state.address,
+      cartItems: this.props.cartItems,
+    };
+    this.props.createOrder(order);
+
+    this.setState({
+      name: "",
+      address: "",
+      email: "",
+    });
+  };
   render() {
     const { cartItems, removeFromCart } = this.props;
+
     console.log(cartItems);
     return (
       <div>
@@ -50,12 +83,70 @@ export default class Cart extends Component {
                     {cartItems.reduce((a, c) => a + c.price * c.count, 0)}
                   </div>
                   <div>
-                    <button className="btn btn-primary">Proceed</button>
+                    <button
+                      onClick={this.handleClick}
+                      className={`btn  ${
+                        this.state.showCheckout ? "btn-info" : "btn-success"
+                      }`}
+                    >
+                      {this.state.showCheckout ? "Go Back" : "Proceed"}
+                    </button>
                   </div>
                 </div>
               </>
             )}
           </div>
+          {this.state.showCheckout && (
+            <div className="cart cart__form">
+              <form onSubmit={this.createOrder}>
+                <ul className="form__container">
+                  <li>
+                    <label>Email</label>
+                    <input
+                      value={this.state.email}
+                      name="email"
+                      type="email"
+                      required
+                      onChange={this.handleInput}
+                    />
+                  </li>
+                  <li>
+                    <label>Name</label>
+                    <input
+                      value={this.state.name}
+                      name="name"
+                      type="text"
+                      required
+                      onChange={this.handleInput}
+                    />
+                  </li>
+                  <li>
+                    <label>Address</label>
+                    <input
+                      value={this.state.address}
+                      name="address"
+                      type="text"
+                      required
+                      onChange={this.handleInput}
+                    />
+                  </li>
+                  <li>
+                    <button
+                      type="submit"
+                      className="btn btn-success"
+                      disabled={
+                        !this.state.name &&
+                        !this.state.email &&
+                        !this.state.address
+                      }
+                    >
+                      Check Out
+                    </button>
+                  </li>
+                </ul>
+              </form>
+            </div>
+          )}
         </div>
       </div>
     );
